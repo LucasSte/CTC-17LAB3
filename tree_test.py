f2df140c4edfb719e94a09c7908fba542ac2b243
LucasSte <lucas.tnagel@gmail.com>
2021-01-13 19:58:58 -0300

    Add comments to main file

--- tree_test.py
+++ tree_test.py
@@ -4,15 +4,23 @@ from ID3 import ID3Algorithm
 from apriori import AprioriClassifier
 import json
 
+# Read csv file from path
 path = os.getcwd()
 df = pd.read_csv(path + '/accident_data.csv')
 
 del df['Data']
 
+# Choose variable to classify
 tree_builder = ID3Algorithm('Accident Level')
 
-print(tree_builder.gain_list(df))
+inf_gains = tree_builder.gain_list(df)
+print('Information gains:')
+for elem in inf_gains:
+    print(elem)
 
+print()
+
+# Delete variables with low information gain
 del df['Industry Sector']
 del df['Employee ou Terceiro']
 # del df['Risco Critico']
@@ -24,8 +32,10 @@ tree_builder.build_tree(df)
 apriori_class = AprioriClassifier(df, 'Accident Level')
 
 # print(tree_builder.tree)
+print('Decision tree:')
 print(json.dumps(tree_builder.tree, indent=4))
-print(apriori_class.prediction)
+print()
+print('Apriori classifier=> mode of accident levels data: ', apriori_class.prediction)
 
 input_test = {
     'Potential Accident Level': 'III',
@@ -39,4 +49,4 @@ input_test = {
     'Risco Critico': 'Cut'
 }
 
-print(tree_builder.predict(input_test))
+print('Predicted accident level for example: ', tree_builder.predict(input_test))
